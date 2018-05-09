d72ddf863282850b6a7a2ff823d5ffd0d53e8743
falko <falko.moeller@gmail.com>
2018-05-09 10:32:40 +0200

    Swiping via PageViewController done

---
+++ ScrumArtifacts/PageViewController.swift
@@ -0,0 +1,55 @@
+//
+//  PageViewController.swift
+//  ScrumArtifacts
+//
+//  Created by Falko on 08.05.18.
+//  Copyright © 2018 Falko Möller. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource{
+    
+    var pages = [UIViewController]()
+    
+    // store viewcontroller in pages array
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // tells UIPageViewController that self is its dataSourceDelegate
+        dataSource = self
+
+        pages.append(UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "fiboOne"))
+        pages.append(UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "fiboTwo"))
+        setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
+    }
+    
+    // What should be shown before
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        // shouldnt be empty check
+        guard let index = pages.index(of: viewController) else {
+            return nil
+        }
+        
+        if index == 0 {
+            return pages.last
+        }
+        return pages[index-1]
+        
+    }
+    
+    // What should be shown next
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+       
+        guard let index = pages.index(of: viewController) else {
+            return nil
+        }
+        
+        if index == pages.count-1 {
+            return pages.first
+        }
+        return pages[index+1]
+    }
+ 
+}
